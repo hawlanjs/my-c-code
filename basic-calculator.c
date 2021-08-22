2f652fcca336d298eed93f727503aaf71595358b
hawlan.js <83552463+hawlanjs@users.noreply.github.com>
2021-08-22 02:52:28 -0700

    Create basic-calculator.c

---
+++ basic-calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+	
+	int num1;
+	int num2;
+	printf("Enter first number: ");
+	scanf("%d", &num1);
+	printf("Enter second number: ");
+	scanf("%d", &num2);
+
+	printf("Answer: %d", num1 + num2);
+
+
+
+	return 0;
+}
