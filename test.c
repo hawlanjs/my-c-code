1b91772996b91999ffff692093532c3906d77ce0
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-09-23 08:34:31 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+   int a = 10; 
+   printf("%i\n", sizeof(a));
+   char b = 'a';
+   printf("%i\n", sizeof(b));
+   double c = 10.5;
+   printf("%i\n", sizeof(c));
+   float d = 10.5;
+   printf("%i\n", sizeof(d));
+}
