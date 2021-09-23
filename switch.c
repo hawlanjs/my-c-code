89b09c92a9d83134c31e6198f96196b90c6d8829
hawlan.js <83552463+hawlanjs@users.noreply.github.com>
2021-09-23 11:02:28 -0700

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("how many pizza slices you eat (tell the truth):");
+   int slices;
+   scanf("%d", &slices);
+   int caloriespreSlice = 250;
+
+
+  switch(slices)
+  {
+
+   case 1:
+         printf("you did a great job!");
+        break;
+   case 2:
+         printf("you did an ok job!");
+        break;
+   case 3:
+        printf("you did an ok job! try to eat 2 next time");
+        break;
+   case 4:
+        printf("you're a dissapointment");
+        break;
+   default:
+         printf("enjoy your heart disease");
+         break;
+
+  }
+
+  printf("you had %d calories.\n", caloriespreSlice * slices);
+
+  return 0;
+
+  
+}
