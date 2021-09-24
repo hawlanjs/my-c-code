125706e7c3bfdc577d674814bfdee9050b6efe54
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-09-24 10:16:37 -0400

    Create If_Example.c

---
+++ If_Example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 100;
+    int b = 10;
+    int c = 10;
+    
+    int d = b * c;
+    if (d == a)
+        printf("This number is equal");
+        
+    d = a + b - c;
+    if (a == d)
+        printf("\nhooora you are the best you can trase this code");
+
+    return 0;
+}
