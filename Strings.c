0dd4f383ee1964c1c2a0872593b542aa67bd1ada
hawlan <83552463+hawlanjs@users.noreply.github.com>
2021-10-09 06:54:28 -0700

    Create  Strings.c

---
+++ Strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[31];
+    printf("yo gurl enter yo name: ");
+    scanf("%s", name);
+    printf("hello %s!\nyou look very nice today.\n", name); 
+ 
+
+
+    return 0;
+}
